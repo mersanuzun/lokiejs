423349d55e94b7250e17727ec26dcc1e201de659
Mehmet Ersan Uzun <mersanuzun@gmail.com>
2021-01-02 17:40:35 +0300

    Add test

---
+++ __tests__/LokieJs.spec.ts
@@ -0,0 +1,206 @@
+import LokieJS, {LokieObject, SetItemOptions} from "../src/LokieJS";
+import * as faker from 'faker';
+import { when } from 'jest-when';
+
+describe('LokieJS', () => {
+  let localStorageMock;
+
+  beforeAll(() => {
+    localStorageMock = {};
+    localStorageMock.__proto__ = {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      removeItem: jest.fn(),
+      clear: jest.fn(),
+    };
+
+    Object.defineProperty(global, 'localStorage', {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      value: localStorageMock,
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  describe('setItem', () => {
+    it('should set undefined to expire field if not give expire time', () => {
+      // given
+      const key = faker.random.word();
+      const value = faker.random.objectElement();
+      const object: LokieObject<any> = {
+        data: value,
+      };
+
+      // when
+      LokieJS.setItem(key, value);
+
+      // then
+      const [ [calledKey, calledObjAsString] ] = localStorageMock.setItem.mock.calls;
+
+      expect(localStorageMock.setItem).toHaveBeenCalledTimes(1);
+      expect(calledKey).toBe(key);
+      expect(JSON.parse(calledObjAsString)).toEqual(object);
+    });
+    it('should set to expire time from getTime method if expire time is given as Date', () => {
+      // given
+      const key = faker.random.word();
+      const value = faker.random.objectElement();
+      const expireDate = new Date();
+      jest.spyOn(expireDate, 'getTime').mockReturnValue(1);
+      const setItemOptions: SetItemOptions = {
+        expire: expireDate,
+      };
+      const object: LokieObject<any> = {
+        data: value,
+        expire: 1,
+      };
+
+      // when
+      LokieJS.setItem(key, value, setItemOptions);
+
+      // then
+      const [ [calledKey, calledObjAsString] ] = localStorageMock.setItem.mock.calls;
+
+      expect(localStorageMock.setItem).toHaveBeenCalledTimes(1);
+      expect(calledKey).toBe(key);
+      expect(JSON.parse(calledObjAsString)).toEqual(object);
+    });
+    it('should set to expire time if expire time is given as number', () => {
+      // given
+      const key = faker.random.word();
+      const value = faker.random.objectElement();
+      const setItemOptions: SetItemOptions = {
+        expire: 3,
+      };
+      const object: LokieObject<any> = {
+        data: value,
+        expire: setItemOptions.expire,
+      };
+
+      // when
+      LokieJS.setItem(key, value, setItemOptions);
+
+      // then
+      const [ [calledKey, calledObjAsString] ] = localStorageMock.setItem.mock.calls;
+
+      expect(localStorageMock.setItem).toHaveBeenCalledTimes(1);
+      expect(calledKey).toBe(key);
+      expect(JSON.parse(calledObjAsString)).toEqual(object);
+    });
+  });
+
+  describe('getItem', () => {
+    beforeEach(() => {
+      jest.spyOn(global.console, 'warn');
+    });
+
+    it('should return null if any error occurred while parsing retrieved object', () => {
+      // given
+      const key = faker.random.word();
+      when(localStorageMock.getItem)
+        .calledWith(key)
+        .mockReturnValue(faker.random.word());
+
+      // when
+      const value = LokieJS.getItem(key);
+
+      // then
+      expect(value).toEqual(null);
+      expect(console.warn).toHaveBeenNthCalledWith(
+        1,
+        `Could not get item from localstorage with ${key}`
+      );
+    });
+    it('should return null if value was expired', () => {
+      // given
+      const lokieObject: LokieObject<any> = {
+        data: false,
+        expire: 10,
+      };
+
+      const key = faker.random.word();
+      when(localStorageMock.getItem)
+        .calledWith(key)
+        .mockReturnValue(JSON.stringify(lokieObject));
+      const mockDate = new Date(lokieObject.expire.valueOf() + 1);
+      jest
+        .spyOn(global, 'Date')
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        .mockImplementation(() => mockDate);
+
+      // when
+      const value = LokieJS.getItem(key);
+
+      // then
+      expect(value).toEqual(null);
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(localStorageMock.removeItem).toHaveBeenNthCalledWith(
+        1,
+        key
+      );
+    });
+    it('should return value if expire not set before', () => {
+      // given
+      const lokieObject: LokieObject<any> = {
+        data: false,
+      };
+      const key = faker.random.word();
+      when(localStorageMock.getItem)
+        .calledWith(key)
+        .mockReturnValue(JSON.stringify(lokieObject));
+
+      // when
+      const value = LokieJS.getItem(key);
+
+      // then
+      expect(value).toEqual(lokieObject.data);
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(localStorageMock.removeItem).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('sync', () => {
+    it('should sync all key value pairs if excludeKeys is not given as parameter', () => {
+      // given
+      localStorageMock.key1 = 'value1';
+      localStorageMock.key2 = 'value2';
+      when(localStorageMock.getItem)
+        .calledWith('key1')
+        .mockReturnValue('value1');
+      when(localStorageMock.getItem)
+        .calledWith('key2')
+        .mockReturnValue('value2');
+
+      // when
+      LokieJS.sync();
+
+      // then
+      expect(localStorageMock.setItem).toHaveBeenCalledWith('key1', '{"data":"value1"}');
+      expect(localStorageMock.setItem).toHaveBeenCalledWith('key2', '{"data":"value2"}');
+    });
+
+    it('should sync only not excluded key value pairs if excludeKeys is given as parameter', () => {
+      // given
+      localStorageMock.key1 = 'value1';
+      localStorageMock.key2 = 'value2';
+      when(localStorageMock.getItem)
+        .calledWith('key1')
+        .mockReturnValue('value1');
+      when(localStorageMock.getItem)
+        .calledWith('key2')
+        .mockReturnValue('value2');
+
+      // when
+      LokieJS.sync(['key1']);
+
+      // then
+      expect(localStorageMock.setItem).toHaveBeenCalledWith('key2', '{"data":"value2"}');
+      expect(localStorageMock.setItem).toHaveBeenCalledTimes(1);
+
+    });
+  });
+});
---
+++ src/LokieJS.ts
@@ -0,0 +1,65 @@
+type Expire = number | Date;
+export interface SetItemOptions {
+  expire?: Expire,
+}
+export interface LokieObject<T> {
+  expire?: Expire
+  data: T
+}
+export interface LokieJS {
+  setItem<T>(key: string, value: T, options?: SetItemOptions): void
+  getItem<T> (key: string): T | null
+  sync(excludeKeys?: string[]): void
+}
+
+const LokieJS: LokieJS = {
+  setItem<T>(key: string, value: T, options?: SetItemOptions): void {
+    let exp: Expire;
+
+    if (options?.expire instanceof Date) {
+      exp = options.expire.getTime();
+    } else if (typeof options?.expire === 'number') {
+      exp = options.expire;
+    }
+
+    const lokieObject: LokieObject<T> = {
+      expire: exp,
+      data: value,
+    };
+
+    localStorage.setItem(key, JSON.stringify(lokieObject));
+  },
+
+  getItem<T>(key: string): T | null {
+    const now = (new Date()).getTime();
+    const objectAsString = localStorage.getItem(key);
+    try {
+      const object: LokieObject<T> = JSON.parse(objectAsString);
+
+      if (object.expire && object.expire < now) {
+        localStorage.removeItem(key);
+
+        return null;
+      }
+
+      return object.data;
+    } catch (err) {
+      console.warn(`Could not get item from localstorage with ${key}`);
+
+      return null;
+    }
+  },
+
+  sync(excludeKeys?: string[]): void {
+    Object.keys(localStorage).forEach(key => {
+      const value = localStorage.getItem(key);
+
+      if (!excludeKeys?.includes(key)) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+        this.setItem(key, value);
+      }
+    });
+  },
+};
+
+export default LokieJS;
